5ec307561dab04d9395113f1ad75efc25b3e2793
KatrinaMarks <kmarks@vols.utk.edu>
2022-10-09 13:06:11 -0400

    Working on issue #7

--- recipe_box/base/views.py
+++ recipe_box/base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 
@@ -23,5 +23,11 @@ def all_sections(request):
     return render(request, "all_sections.html")
 
 def account(response):
-    form = UserCreationForm()
+    if response.method == "POST":
+        form = UserCreationForm(response.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("home")
+    else:
+        form = UserCreationForm()
     return render(response, "account.html", {"form": form})
