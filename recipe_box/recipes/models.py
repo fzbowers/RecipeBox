7323fa25055843b2b1309cca378ba7c92a446c14
Paige King <kingpaige1305@gmail.com>
2022-10-10 19:09:12 -0400

    working on #10

--- recipe_box/recipes/models.py
+++ recipe_box/recipes/models.py
@@ -1,3 +1,11 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class Ingredient(models.Model):
+    body = models.CharField(max_length = 100)
+    
+class Recipes(models.Model):
+    title = models.CharField(max_length = 50)
+    ingredients = models.ManyToManyField("Ingredient")
