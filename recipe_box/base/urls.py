0c51a03d0bdd5ad77cca652e1502b754e1670afe
KatrinaMarks <kmarks@vols.utk.edu>
2022-10-11 23:05:07 -0400

    Working on issue #9

--- recipe_box/base/urls.py
+++ recipe_box/base/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("new_section/", views.new_section, name="new_section"),
     path("all_sections/", views.all_sections, name="all_sections"),
     path("account/", views.account, name="account"),
+    path("create_account/", views.create_account, name="create_account")
 ]
