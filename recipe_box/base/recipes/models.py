f28f3b15dfaf18407ff33dc615a42079aec3274b
Paige King <kingpaige1305@gmail.com>
2022-10-12 21:02:47 -0400

    added #12

--- recipe_box/base/recipes/models.py
+++ recipe_box/base/recipes/models.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 from unittest.util import _MAX_LENGTH
 from django.db import models
 
@@ -6,6 +7,37 @@ from django.db import models
 class Ingredient(models.Model):
     body = models.CharField(max_length = 100)
     
+    class Meta:
+        verbose_name = ('Ingredient')
+        verbose_name_plural = ('Ingredients')
+
 class Recipes(models.Model):
     title = models.CharField(max_length = 50)
     ingredients = models.ManyToManyField("Ingredient")
+
+    class Meta:
+        ordering = ['title']
+        verbose_name = ('Recipe')
+        verbose_name_plural = ('Recipies')
+
+class Instruction(models.Model):
+
+    text = models.TextField(blank=True, verbose_name=('Instrution'))
+    recipe = models.ForeignKey(Recipes, related_name='instructions', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = ('Instruction')
+        verbose_name_plural = ('Instructions')
+        ordering = ['order', 'id']
+
+class Section(models.Model):
+    name = models.CharField(max_length=120,
+                            unique=True,
+                            verbose_name=('Section'))
+    slug = models.SlugField(unique=True, help_text='Automatically generated from the title')
+    order_index = models.PositiveIntegerField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = ('Section')
+        verbose_name_plural = ('Sections')
+        ordering = ['order_index']
