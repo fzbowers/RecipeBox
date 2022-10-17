9017af7bf5799ee707f7e3688bf1399d66086f2c
Fatima Bowers <93776953+fzbowers@users.noreply.github.com>
2022-10-16 22:52:56 -0400

    Remove all sections page

--- recipe_box/base/urls.py
+++ recipe_box/base/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path("new_recipe/", views.new_recipe, name="new_recipe"),
     path("all_recipes/", views.all_recipes, name="all_recipes"),
     path("new_section/", views.new_section, name="new_section"),
-    path("all_sections/", views.all_sections, name="all_sections"),
     path("account/", views.account, name="account"),
     path("create_account/", views.create_account, name="create_account"),
 ]
