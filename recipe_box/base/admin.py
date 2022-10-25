edc4f4ea28c93ad93f2e0034d01564a600b6b68e
Lili Swann <lili@mycorseflow.com>
2022-10-25 09:05:24 -0400

    setting up search bar #28

--- recipe_box/base/admin.py
+++ recipe_box/base/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Recipes
+
+class RecipesAdmin(admin.ModelAdmin):
+    list_display = ("title", "section",)
+
+admin.site.register(Recipes, RecipesAdmin)
--- recipe_box/register/views.py
+++ recipe_box/register/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import TemplateView, ListView
+
+from base.models import Recipes
 
 # Create your views here.
 def register(response):
     form = UserCreationForm()
     return render(response, "account.html", {"form":form})
+
+
+class SearchResultsView(ListView):
+    model = Recipes
+    template_name = 'search.html'
+
+    def get_queryset(self): # new
+        query = self.request.GET.get("q")
+        object_list = Recipes.objects.filter(
+            Q(title__icontains=query) | Q(section__icontains=query)
+        )
+        return object_list
