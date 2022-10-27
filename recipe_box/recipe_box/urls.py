1504c0cafed523d934c9cce8c3533f7ab9ecd403
KatrinaMarks <kmarks@vols.utk.edu>
2022-10-26 23:52:48 -0400

    Working on issue #29

--- recipe_box/recipe_box/urls.py
+++ recipe_box/recipe_box/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from register import views as v
+from django.contrib.auth import views as auth_views
 #from userinfo.forms import PasswordResetForm 
 
 urlpatterns = [
