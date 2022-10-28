23ce660ede793d19286f1cb31ca18f78d43f8f9e
Paige King <kingpaige1305@gmail.com>
2022-10-27 21:13:05 -0400

    creating views and forms #22

--- recipe_box/base/forms.py
+++ recipe_box/base/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Recipe, Ingredient, Instruction
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -9,3 +10,18 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+class RecipeForm(forms.ModelForm):
+    class Meta:
+        model = Recipe
+        fields = ['name', 'description']
+
+class IngredientForm(forms.ModelForm):
+    class Meta:
+        model = Ingredient
+        fields = ['name', 'quantity', 'unit']
+
+class InstructionForm(forms.ModelForm):
+    class Meta:
+        model = Instruction
+        fields = ['text']
