31f4d36d30d4f3d47dbaa74ce1eca0cd259fa4ca
Lili Swann <lili@mycorseflow.com>
2022-11-08 14:31:15 -0500

    #28

--- recipe_box/base/views.py
+++ recipe_box/base/views.py
@@ -11,10 +11,11 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import PasswordResetForm
 from django.contrib.auth.models import User
 from django.template.loader import render_to_string
-from django.db.models.query_utils import Q
+#from django.db.models.query_utils import Q
 from django.utils.http import urlsafe_base64_encode
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.encoding import force_bytes
+from django.db.models import Q
 
 from .models import Recipe, Ingredient, Instruction, Section
 
@@ -26,10 +27,10 @@ class SearchResultsView(ListView):
     model = Recipe
     template_name = 'search.html'
 
-    def get_queryset(self): # new
+    def get_queryset(self): 
         query = self.request.GET.get("q")
         object_list = Recipe.objects.filter(
-            Q(title__icontains=query) | Q(section__icontains=query)
+            Q(name__icontains=query)  | Q(description__icontains=query)
         )
         return object_list
 
