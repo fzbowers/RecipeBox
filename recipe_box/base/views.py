e5f594a168c5c621cfe5c83c3704f4f7c4df01cb
Lili Swann <lili@mycorseflow.com>
2022-11-09 15:44:36 -0500

    #46

--- recipe_box/base/views.py
+++ recipe_box/base/views.py
@@ -40,7 +40,6 @@ class SearchResultsView(ListView):
 @login_required(login_url="/login")
 def home(request):
     section_qs = Section.objects.filter(user=request.user)
-
     pinned_qs = Recipe.objects.filter(Q(user=request.user) & Q(pinned=True))
 
     context = {
