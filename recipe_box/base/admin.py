53159af31f4169fe23aedd0874bcbc10c6e249f2
Lili Swann <lili@mycorseflow.com>
2022-11-08 19:33:43 -0500

    #45

--- recipe_box/base/admin.py
+++ recipe_box/base/admin.py
@@ -1,18 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
 # Register your models here.
+
+User = get_user_model() ## MIGHT NOT NEED
 
 #display recipies in admin page 
 from .models import Recipe, Section, Ingredient, Instruction
 
+class IngredientsInline(admin.StackedInline):
+    model = Ingredient
+    extra = 0
+
 class RecipesAdmin(admin.ModelAdmin):
-    list_display = ("name", "section",)
+    inlines = [IngredientsInline]
+    list_display = ['user', 'name']
+    ## , 'section']
+    raw_id_fields = ['user']
 
 class SectionAdmin(admin.ModelAdmin):
-    list_display = ("name",)
+    list_display = ['name']
+    raw_id_fields = ['user']
     
 admin.site.register(Recipe, RecipesAdmin)
 admin.site.register(Section, SectionAdmin)
-admin.site.register(Ingredient)
 admin.site.register(Instruction)
 
--- recipe_box/base/models.py
+++ recipe_box/base/models.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from tabnanny import verbose
 from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.utils.text import slugify
+from django.urls import reverse
 
 # Create your models here.
 
@@ -22,20 +24,37 @@ class Section(models.Model):
 class Recipe(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length = 150)
-    description = models.TextField(null=True, blank=True)
+    slug = models.SlugField(blank=True, null=True)
+    ## description = models.TextField(null=True, blank=True) ## DON"T NEED
     time_to_make = models.CharField(max_length = 25, default=0)
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    section = models.ForeignKey(Section, on_delete=models.PROTECT)
+    ## section = models.ForeignKey(Section, on_delete=models.PROTECT)
     pinned = models.BooleanField(default=False)
 
     class Meta:
         verbose_name = ('Recipe')
         verbose_name_plural = ('Recipes')
-    
+
+    def get_ingredients_children(self):
+        return self.ingredient_set.all()
+
+    '''
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if self.slug is None:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+    '''
+
+    def get_absolute_url(self):
+        return reverse("individual_recipe", kwargs={"title": self.name})
         
+    def get_edit_url(self):
+        return reverse("edit_recipe", kwargs={"title": self.name})
+
 
 class Ingredient(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
@@ -46,9 +65,13 @@ class Ingredient(models.Model):
         verbose_name = ('Ingredient')
         verbose_name_plural = ('Ingredients')
     
+    '''
     def __str__(self):
         return self.name
+    '''
 
+    def get_absolute_url(self):
+        return self.recipe.get_absolute_url()
 
 class Instruction(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
