8c70564fcacddb69c8a39479d52da814c09de334
Lili Swann <lili@mycorseflow.com>
2022-11-10 07:59:22 -0500

    #46

--- recipe_box/base/urls.py
+++ recipe_box/base/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path("account/", views.account, name="account"),
     path("create_account/", views.create_account, name="create_account"),
     path("password_reset/", views.password_reset_request, name="password_reset"),
+    path("change_password/", views.change_password, name='change_password'),
 ]
