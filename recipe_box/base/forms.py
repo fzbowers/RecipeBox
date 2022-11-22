ddeed3dd08124795bb0e4d3a381e2a60d3d8e17a
Fatima Bowers <bowersfatima@gmail.com>
2022-11-22 09:05:48 -0500

    small changes

--- recipe_box/base/forms.py
+++ recipe_box/base/forms.py
@@ -65,7 +65,7 @@ class InstructionForm(forms.ModelForm):
         fields = ['text']
 
 class ShoppingForm(forms.ModelForm):
-    name = forms.CharField(label='Food', label_suffix="", widget=forms.TextInput(attrs={'id' : "shopping", 'class' : "col-sm-6"}))
+    name = forms.CharField(label='Item', label_suffix="", widget=forms.TextInput(attrs={'id' : "shopping", 'class' : "col-sm-6"}))
     quantity = forms.CharField(label='Quantity', label_suffix="", widget=forms.TextInput(attrs={'id' : "shopping", 'class' : "col-sm-2"}))
     class Meta:
         model = Food
